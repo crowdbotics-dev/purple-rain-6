53851ba39f0491aa0250714112056fd97b65f56e
Crowdbotics <engineering@crowdbotics.com>
2023-02-28 00:29:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-rain-6",
+  url: "https://purple-rain-6.botics.co",
+  api: "https://purple-rain-6.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
